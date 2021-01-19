c275c6b0d589d47a5e4d337593bf662cac83fc62
gali <li_jiali@sina.com>
2021-01-19 13:28:35 +0800

    second commit

---
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.renren.modules.sys.controller;
+
+
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.Producer;
+import io.renren.common.utils.R;
+import io.renren.modules.sys.entity.SysUserEntity;
+import io.renren.modules.sys.service.SysUserService;
+import io.renren.modules.sys.service.SysUserTokenService;
+import io.renren.modules.sys.shiro.ShiroUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.crypto.hash.Sha256Hash;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * 登录相关
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Controller
+public class SysLoginController {
+	@Autowired
+	private Producer producer;
+	@Autowired
+	private SysUserService sysUserService;
+	@Autowired
+	private SysUserTokenService sysUserTokenService;
+
+	@RequestMapping("captcha.jpg")
+	public void captcha(HttpServletResponse response)throws IOException {
+        response.setHeader("Cache-Control", "no-store, no-cache");
+        response.setContentType("image/jpeg");
+
+        //生成文字验证码
+        String text = producer.createText();
+        //生成图片验证码
+        BufferedImage image = producer.createImage(text);
+        //保存到shiro session
+        ShiroUtils.setSessionAttribute(Constants.KAPTCHA_SESSION_KEY, text);
+        
+        ServletOutputStream out = response.getOutputStream();
+        ImageIO.write(image, "jpg", out);
+	}
+	
+	/**
+	 * 登录
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/sys/login", method = RequestMethod.POST)
+	public R login(String username, String password, String captcha) {
+		String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
+		if(!captcha.equalsIgnoreCase(kaptcha)){
+			return R.error("验证码不正确");
+		}
+
+		//用户信息
+		SysUserEntity user = sysUserService.queryByUserName(username);
+
+		//账号不存在、密码错误
+		if(user == null || !user.getPassword().equals(new Sha256Hash(password, user.getSalt()).toHex())) {
+			return R.error("账号或密码不正确");
+		}
+
+		//账号锁定
+		if(user.getStatus() == 0){
+			return R.error("账号已被锁定,请联系管理员");
+		}
+
+		//生成token，并保存到数据库
+		R r = sysUserTokenService.createToken(user.getUserId());
+		return r;
+	}
+	
+	/**
+	 * 退出
+	 */
+	@RequestMapping(value = "logout", method = RequestMethod.GET)
+	public String logout() {
+		ShiroUtils.logout();
+		return "redirect:login.html";
+	}
+
+	public static void main(String[] args) {
+		String s = new Sha256Hash("admin", "YzcmCZNvbXocrsz9dm8e").toHex();
+		System.out.println(s);
+	}
+}
